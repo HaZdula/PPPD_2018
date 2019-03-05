1037a9ff7a3f0e75417816475ecd462e81d03017
HaZdula <43793964+HaZdula@users.noreply.github.com>
2019-03-05 19:30:01 +0100

    PDU

---
+++ pdu/skrytp1.R
@@ -0,0 +1,19 @@
+# ctrl + shift + c komentarz
+
+
+2 + 2
+# ctr + enter ewaluacja
+
+x <- c(1,2,3,4,5,12,3) # c - combine
+
+print(x)
+plot(x)
+yy <- c(1,35,4)
+print(yy)
+source('~/pdu/skrypt2.R')
+
+f <- function(x){
+    x^2   #x**2
+}
+
+f(x)
